ce9db07c8c3f5186aa4bebc3111f61860a03e74c
camillastrauss <camilla.k.strauss@gmail.com>
2025-07-14 13:46:24 -0400

    eda typo

--- src/MOBI_QC/important_files/eda_qc.py
+++ src/MOBI_QC/important_files/eda_qc.py
@@ -181,7 +181,7 @@ def eda_qc(xdf_filename: str, stim_df:pd.DataFrame, task='RestingState') -> tupl
     subject = xdf_filename.split('sub-')[1].split('/')[0]
     whole_ps_df = import_physio_data(xdf_filename)
     vars = {}
-    vars['sampling_rate'], vars['signal_integrity_check'], vars['average_scl'], vars['scl_sd'], vars['scl_cv'], vars['average_scr_amplitude'], vars['sc_validity'], vars['snr'] = np.zeros(8)
+    vars['sampling_rate'], vars['signal_integrity_check'], vars['average_scl'], vars['scl_sd'], vars['scl_cv'], vars['average_scr_amplitude'], vars['scr_validity'], vars['snr'] = np.zeros(8)
 
     try: 
         ps_df = get_event_data(event=task,
@@ -207,7 +207,7 @@ def eda_qc(xdf_filename: str, stim_df:pd.DataFrame, task='RestingState') -> tupl
         print(f"Average Amplitude of Skin Conductance Response: {average_scr_amplitude:.3f} mS")
         vars['average_scr_amplitude'] = average_scr_amplitude
         print(f"Skin Conductance Response Validity: {scr_amplitude_validity:.3f} %")
-        vars['sc_validity'] = scr_amplitude_validity
+        vars['scr_validity'] = scr_amplitude_validity
         print(f"Signal to Noise Ratio: {eda_snr(eda_signals, eda_df, eda_sampling_rate):.3f} dB")
         vars['snr'] = eda_snr(eda_signals, eda_df, eda_sampling_rate)
 
